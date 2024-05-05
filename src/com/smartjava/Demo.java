d371371c717864a891294b3042c99294038b568b
SweetykP <mail2kamleshpatil@gmail.com>
2024-05-05 13:29:38 +0530

    updated code

---
+++ src/com/smartjava/Demo.java
@@ -0,0 +1,41 @@
+package com.smartjava;
+
+public class Demo {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("I Love India....");
+		System.out.println("I Love India....");
+		System.out.println("I Love India....");
+		System.out.println("I Love India....");
+		System.out.println("I Love India....");
+		
+		System.out.println("-----------");
+		//for loop
+		
+		for(int i=0; i<15;i++) {
+			System.out.println("I Love India....");
+		}
+		
+		System.out.println("-----------");
+		
+		for(int i=0; i<5;i++) {
+			System.out.println("Welcome in smart java...");
+		}
+		
+		System.out.println("-----------");
+		
+		for(int i=0; i<5;i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("-----------");
+		
+		int num[]= {60,98,80,81,83};
+		
+		for(int i=0; i<num.length; i++) {
+			System.out.println(num[i]);
+		}
+	}
+
+}
